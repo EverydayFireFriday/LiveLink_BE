6788678da6881ab91fcf4ff10eb5a7d5e1f67eb2
양희범 <sinbla78@gmail.com>
2025-08-08 14:37:29 +0900

    feat ::  docker 및 설정 추가

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'livelink-backend',
+      script: 'dist/app.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+};
