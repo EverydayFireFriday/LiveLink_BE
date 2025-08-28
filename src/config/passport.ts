e37c60015d917e8c59ce3960e24f638842988094
양희범 <sinbla78@gmail.com>
2025-08-28 14:35:52 +0900

    fix :: apple oauth 필드 수정

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -102,7 +102,7 @@ export const configurePassport = (passport: PassportStatic) => {
           clientID: process.env.APPLE_CLIENT_ID,
           teamID: process.env.APPLE_TEAM_ID,
           keyID: process.env.APPLE_KEY_ID,
-          privateKey: process.env.APPLE_PRIVATE_KEY.replace(/\n/g, '\n'),
+          privateKeyString: process.env.APPLE_PRIVATE_KEY.replace(/\n/g, '\n'),
           callbackURL: '/api/auth/apple/callback',
           scope: ['name', 'email'],
         },
