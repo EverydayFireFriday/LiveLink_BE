806deac3bf55b8472e10003dc0da188e9b4c21fe
양희범 <sinbla78@gmail.com>
2025-10-01 07:41:40 +0900

    fix :: 스크마 오류 수정

---
+++ src/config/swagger/schemas/paginationSchema.ts
@@ -0,0 +1,23 @@
+export const paginationSchema = {
+  Pagination: {
+    type: 'object',
+    properties: {
+      page: { type: 'number', example: 1, description: 'Current page number' },
+      limit: {
+        type: 'number',
+        example: 10,
+        description: 'Number of items per page',
+      },
+      totalItems: {
+        type: 'number',
+        example: 100,
+        description: 'Total number of items',
+      },
+      totalPages: {
+        type: 'number',
+        example: 10,
+        description: 'Total number of pages',
+      },
+    },
+  },
+};
