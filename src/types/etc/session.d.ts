ce23b6c4a825cd2d66f0c05ba8e8407bf7afbc54
양희범 <sinbla78@gmail.com>
2025-10-15 19:15:44 +0900

    fix :: 세션 검증 수정

--- src/types/etc/session.d.ts
+++ src/types/etc/session.d.ts
@@ -4,10 +4,22 @@ import 'express-session';
 declare module 'express-session' {
   interface SessionData {
     user?: {
-      email: string;        // 아이디로 사용되는 이메일
       userId: string;
-      username: string;     // 이름(수정 가능)
+      email: string;
+      username: string;
+      name: string;
+      birthDate: Date;
+      status: string;
+      statusReason?: string;
       profileImage?: string;
+      isTermsAgreed: boolean;
+      termsVersion?: string;
+      createdAt: Date;
+      updatedAt: Date;
+      provider?: string;
+      socialId?: string;
+      likedConcerts: string[];
+      likedArticles: string[];
       loginTime: string;
     };
   }
